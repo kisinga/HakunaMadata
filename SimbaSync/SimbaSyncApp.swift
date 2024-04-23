0ee7025f70541a3324b06c9d67dfaeacf789b714
Steve Kisinga <stevekisinga@microsoft.com>
2024-04-23 10:09:11 +0300

    Initial Commit

---
+++ SimbaSync/SimbaSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimbaSyncApp.swift
+//  SimbaSync
+//
+//  Created by Steve Kisinga on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimbaSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
